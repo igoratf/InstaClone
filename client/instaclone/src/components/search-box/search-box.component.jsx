4b948e4ed5d91966ad36d626d05bd6319ab0f492
igor <igor.atf@gmail.com>
2020-06-23 17:21:07 -0300

    change search box layout and add transitions

--- client/instaclone/src/components/search-box/search-box.component.jsx
+++ client/instaclone/src/components/search-box/search-box.component.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react'
-import SearchIcon from '@material-ui/icons/Search';
-import { Container, SearchInput } from './search-box.styles';
+import React, { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+
+import { Container, SearchInput, CancelButton } from "./search-box.styles";
 
 const SearchBox = () => {
-    const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
 
-    const handleInputChange = (event) => {
-        const { value}  = event.target;
-        setSearchText(value);
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setSearchText(value);
 
-        console.log(value);
-    }
+    console.log(value);
+  };
 
-    return (
-        <Container>
-            <SearchIcon style={{fontSize: 14}}/>
-            <SearchInput placeholder="Search" value={searchText} onChange={(e) => handleInputChange(e)} style={{fontSize: 13}}/>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <SearchIcon style={{ fontSize: 14 }} />
+      <SearchInput
+        placeholder="Search"
+        value={searchText}
+        onChange={(e) => handleInputChange(e)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        isFocused={isFocused}
+        style={{ fontSize: 13 }}
+      />
+      <CancelButton isFocused={isFocused} />
+    </Container>
+  );
+};
 
 export default SearchBox;
--- client/instaclone/src/components/search-box/search-box.styles.jsx
+++ client/instaclone/src/components/search-box/search-box.styles.jsx
@@ -1,30 +1,42 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import CancelIcon from "@material-ui/icons/Cancel";
 
 const gray = "rgb(142, 142, 142)";
 
 export const Container = styled.div`
-display: flex;
-align-items: center;
-padding: 8px;
-width: 215px;
-height: 30px;
-min-width: 125px;
-border: 1px solid #dbdbdb;
-background-color: #fafafa;
-color: ${gray};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 8px;
+  width: 215px;
+  height: 30px;
+  min-width: 125px;
+  border: 1px solid #dbdbdb;
+  background-color: #fafafa;
+  color: ${gray};
 `;
 
 export const SearchInput = styled.input`
-background-color: transparent;
-width: 100%;
-padding: 8px;
-height: 10px;
-border: none;
-outline: none;
+  background-color: transparent;
+  width: ${(props) => (props.isFocused ? "100%" : "50px")};
+  padding: 8px;
+  height: 10px;
+  border: none;
+  outline: none;
+  transition: width 0.15s;
 
-&& {
+  && {
     &::placeholder {
-        color: ${gray};
+      color: ${gray};
     }
-}
-`
+  }
+`;
+
+export const CancelButton = styled(CancelIcon)`
+  && {
+    font-size: 14px;
+    position: "absoute";
+    right: 0;
+    visibility: ${(props) => (props.isFocused ? "visible" : "hidden")};
+  }
+`;
