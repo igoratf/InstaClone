be4051d126818b210e8d4252a685c1acaccd257f
igor <igor.atf@gmail.com>
2020-07-14 18:12:48 -0300

    story improvements - add options icon, message box and send button

--- client/instaclone/src/pages/story/story.component.jsx
+++ client/instaclone/src/pages/story/story.component.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import { Container, StoryWrapper, Header, Author } from './story.styles';
+import React, { useState } from "react";
+import {
+  Container,
+  StoryWrapper,
+  Header,
+  Author,
+  StoryOptions,
+  UserInfo,
+  Footer,
+  MessageInput,
+  MessageBox,
+  MessageInputContainer,
+  SendButton,
+  OptionsContainer,
+} from "./story.styles";
 
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from "@material-ui/core/Avatar";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
 const Story = () => {
-    return (
-        <Container>
-            <StoryWrapper>
-                <Header>
-                    <Avatar style={{width: 30, height: 30}}>I</Avatar>
-                    <Author>Name</Author>
-                </Header>
-            </StoryWrapper>
-        </Container>
-    )
-}
+  const [isFocused, setIsFocused] = useState(false);
 
-export default Story
+  return (
+    <Container>
+      <StoryWrapper>
+        <Header>
+          <UserInfo>
+            <Avatar style={{ width: 30, height: 30 }}>I</Avatar>
+            <Author>Name</Author>
+          </UserInfo>
+
+          <OptionsContainer>
+            <StoryOptions />
+          </OptionsContainer>
+        </Header>
+
+        <Footer>
+          <MessageBox>
+            <MessageInputContainer>
+              <MessageInput
+                placeholder="Send a message..."
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+              />
+            </MessageInputContainer>
+          </MessageBox>
+            <SendButton style={isFocused ? {width: 0, opacity: 0} : null}>
+              <SendOutlinedIcon />
+            </SendButton>
+        </Footer>
+      </StoryWrapper>
+    </Container>
+  );
+};
+
+export default Story;
